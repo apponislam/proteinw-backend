f4e9285729850ffb0e2d3dcabe6ccb5da759f978
Appon Islam <appon2003@gmail.com>
2026-08-11 09:45:03 +0600

    Group interface remove gaps

--- src/app/modules/group/group.interface.ts
+++ src/app/modules/group/group.interface.ts
@@ -2,13 +2,11 @@ import { Types } from "mongoose";
 
 export interface IGroup {
     _id?: string;
-
     name: string;
     shortDescription: string;
     goal: number;
     endDate: Date;
     code: string;
-
     runningCampaignId?: Types.ObjectId;
     createdBy?: Types.ObjectId;
     isActive: boolean;
